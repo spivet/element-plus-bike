1a26c28cd25082db85bc0425e9c0da34c21cdb87
spivet <24224596@qq.com>
2021-08-02 14:21:18 +0800

    chore: 初始化组件库

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'alloy',
+    'alloy/vue',
+    'plugin:vue/vue3-recommended',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 2,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
+    }],
+  },
+};
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx({})],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      packages: path.resolve(__dirname, './packages'),
+    },
+  },
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'packages/index.js'),
+      name: 'Plato',
+      filename: 'index.js',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue', 'element-plus'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
