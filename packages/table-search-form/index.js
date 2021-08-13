b2a41155a04a8ccf187cb316bf24cd77e6e31619
spivet <24224596@qq.com>
2021-08-13 14:10:18 +0800

    chore: 删除多余代码

--- packages/table-search-form/index.js
+++
@@ -1,7 +0,0 @@
-import ElProTableSearchForm from './src/index.vue';
-
-ElProTableSearchForm.install = (app) => {
-  app.component(ElProTableSearchForm.name, ElProTableSearchForm);
-};
-
-export default ElProTableSearchForm;
