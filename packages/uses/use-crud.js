a72cbc2b289520ab81333b3d2cf25d3252c356f2
spivet <24224596@qq.com>
2021-08-15 14:54:11 +0800

    feat(useCrud): 增加crud抽象逻辑

---
+++ packages/uses/use-crud.js
@@ -0,0 +1,36 @@
+import { ref, watch } from 'vue';
+
+export default function useCrud(pagination, request) {
+  const initPagination = JSON.parse(JSON.stringify(pagination.value));
+
+  const data = ref([]);
+  const loading = ref(false);
+  const fetchTableData = async () => {
+    loading.value = true;
+    const {tableData, total} = await request();
+    data.value = tableData;
+    pagination.value.total = total;
+    loading.value = false;
+  };
+
+  watch(pagination, () => {
+    fetchTableData();
+  });
+
+  const onSubmit = () => {
+    pagination.value = initPagination;
+  };
+  const onReset = () => {
+    pagination.value = initPagination;
+  };
+
+  fetchTableData();
+
+  return {
+    data,
+    loading,
+    onSubmit,
+    onReset,
+    fetchTableData
+  };
+}
