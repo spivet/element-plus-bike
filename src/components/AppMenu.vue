a64f6fba8fce169baa76403ffb727dcffe127332
spivet <24224596@qq.com>
2021-08-31 11:13:12 +0800

    fix: 刷新页面，选中菜单和路由对应不上

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <el-menu
-    default-active="form"
+    :default-active="activeRoute"
     class="el-menu-vertical-demo"
     :router="true"
   >
@@ -8,16 +8,26 @@
       <template #title>
         数据
       </template>
-      <el-menu-item index="form">
+      <el-menu-item index="/form">
         表单
       </el-menu-item>
-      <el-menu-item index="table">
+      <el-menu-item index="/table">
         表格
       </el-menu-item>
     </el-menu-item-group>
   </el-menu>
 </template>
 
+<script setup>
+import {ref, watch} from 'vue';
+import {useRoute} from 'vue-router';
+
+const activeRoute = ref('');
+const route = useRoute();
+watch(route, (val) => {
+  activeRoute.value = val.path;
+});
+</script>
 
 <style scoped>
 .el-menu {
