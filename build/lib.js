007658347b6e819cebb9feefc06a4273e3d8cc28
spivet <24224596@qq.com>
2021-09-02 18:23:55 +0800

    chore: 分离构建模式，分别打包文档和库

---
+++ build/lib.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  lib: {
+    entry: path.resolve(__dirname, 'packages/index.js'),
+    name: 'Plato',
+    filename: 'index.js',
+  },
+  rollupOptions: {
+    // 确保外部化处理那些你不想打包进库的依赖
+    external: ['vue', 'element-plus'],
+    output: {
+      // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+      globals: {
+        vue: 'Vue',
+      },
+    },
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import { vite2Ext } from "apite";
 
 const path = require('path');
+const lib = require('./build/lib');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,21 +24,5 @@ export default defineConfig({
     },
   },
 
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'packages/index.js'),
-      name: 'Plato',
-      filename: 'index.js',
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  build: process.env.MODE === 'lib' ? lib : {},
 });
