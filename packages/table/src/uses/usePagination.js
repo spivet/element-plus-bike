4b0726b801061114cdf70875c7475394122d9f32
spivet <24224596@qq.com>
2021-09-14 17:52:27 +0800

    feat(table): 增加分页组件全局配置功能

--- packages/table/src/uses/usePagination.js
+++ packages/table/src/uses/usePagination.js
@@ -1,11 +1,14 @@
 import { computed } from 'vue';
+import { getConfig } from 'packages/utils/config';
 
 export default function usePagination($emit, pagination) {
+  const paginationConfig = getConfig('pagination');
   const paginationProps = computed(() => {
     const paginationVal = pagination.value;
     return {
       background: true,
       layout: 'prev, pager, next, sizes, jumper',
+      ...paginationConfig,
       ...paginationVal,
     };
   });
